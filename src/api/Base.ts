557e7bf0339b33fa8336e89e9479abcd7538f081
Lương Văn Tú <87352056+luongvantuit@users.noreply.github.com>
2022-05-08 04:55:05 +0700

    define api

--- src/api/Base.ts
+++ src/api/Base.ts
@@ -1,1 +1,26 @@
-export function fetchApi() {}
+export async function fetchApi<T, E = any>({
+    path,
+    body,
+    headers,
+    method = "GET",
+}: {
+    path: string;
+    body?: any;
+    headers?: HeadersInit | undefined;
+    method?: "GET" | "POST" | "PUT" | "DELETE";
+}): Promise<T | E> {
+    const urlApi: string =
+        process.env.REACT_APP_URL_API || "https://hotel.rogo.com.vn";
+    const versionApi: string = process.env.REACT_VERSION_API || "v1";
+    const response = await fetch(`${urlApi}/${versionApi}/${path}`, {
+        method: method,
+        body: body,
+        headers: headers,
+    });
+
+    const data = await response.json();
+    if (!data["success"]) {
+        return data["msg"];
+    }
+    return (await response.json()) as T;
+}
