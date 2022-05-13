9f61e09e981bf60abcd1dcbdaabeafb033d535a2
Lương Văn Tú <87352056+luongvantuit@users.noreply.github.com>
2022-05-13 16:17:27 +0700

    link base

--- manager/src/screens/ContactScreen.tsx
+++ manager/src/screens/ContactScreen.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { HeaderNav } from "../components/HeaderNav";
 
 export const ContactScreen = React.memo(() => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <HeaderNav navActive="contact" />
+        </React.Fragment>
+    );
 });
